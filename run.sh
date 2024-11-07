63a601524aaed1307f444d250a0adea194c26843
Jiankun <jkzhao.nku@gmail.com>
2024-11-08 00:01:00 +0800

    Modified Script

--- run.sh
+++ run.sh
@@ -7,8 +7,7 @@ export PYTHONPATH="/mnt/users/hccl.local/jkzhao/projects/WavTokenizer":$PYTHONPA
 #     magna_tag_a_tune irmas medleydb esc50 us8k fsd50k vivae
 
 python evaluate_wavtokenizer.py --model wavtokenizer --device cuda \
-    --max_epochs 200 --verbose --tsv_logging_file results/wavtokneizer.tsv \
+    --max_epochs 10 --verbose --tsv_logging_file results/wavtokneizer.tsv \
     --n_iters 1 --data_config_file configs/data_config.json \
-    --enabled_datasets medleydb esc50 us8k fsd50k vivae
-
-# slurp audio_mnist fma_small magna_tag_a_tune irmas
+    --enabled_datasets  ravdess slurp emovo audio_mnist fma_small \
+    magna_tag_a_tune irmas medleydb esc50 us8k fsd50k vivae
